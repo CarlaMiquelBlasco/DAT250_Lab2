de90bb90429ffd49bd8739f33f236a3c9c6dd56b
CarlaMiquel <carla.miquel@estudiantat.upc.edu>
2024-09-08 20:06:19 +0200

    Test class.

--- src/main/java/com/example/demo/PollAppApplication.java
+++ src/main/java/com/example/demo/PollAppApplication.java
@@ -10,18 +10,3 @@ public class PollAppApplication {
 		SpringApplication.run(PollAppApplication.class, args);
 	}
 }
-
-/*
-Step 2: Domain Model
-
-Implement the first draft of the PollApp domain model,
-shown in the diagram above, in Java. For the time being,
-the domain classes can be simple Java Beans, i.e. classes with
-little internal logic, public getters- and setters for all properties,
-as well as a parameterless public constructor. Moreover, you should create
-a PollManager-class, which will manage all domain objects in memory,
-e.g. by holding Polls and Users in a HashMap. The DomainManager
-should be annotated with the @Component annotation, which will allow
-to inject it into the controller classes.
-
-*/
